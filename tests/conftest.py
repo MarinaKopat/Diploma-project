b3259f658f89c7e60f44f9fe690cdfb14a64889e
MarinaKopat <marinakopat@gmail.com>
2026-04-27 18:17:06 +0300

    File

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,23 +13,6 @@ from tests.api.api_client import ApiClient
 load_dotenv()
 
 
-def pytest_sessionstart(session):
-    folders_to_clean = ["data", "allure-results"]
-    for folder in folders_to_clean:
-        if os.path.exists(folder):
-            for filename in os.listdir(folder):
-                file_path = os.path.join(folder, filename)
-                try:
-                    if os.path.isfile(file_path) or os.path.islink(file_path):
-                        os.unlink(file_path)
-                    elif os.path.isdir(file_path):
-                        shutil.rmtree(file_path)
-                except Exception as e:
-                    print(f'Не удалось удалить объект {file_path}. Причина: {e}')
-        else:
-            os.makedirs(folder, exist_ok=True)
-
-
 @pytest.fixture(scope="function")
 def api_admin(page, test_config):
     base_url = test_config["base_url"]
